64c89bcb9e78c44b708d607e3e95cb8f9645a4f1
Natanim-Kemal <kemalnatanim@gmail.com>
2024-01-02 11:31:02 +0300

    made some changes

---
+++ backend/db/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const db = async () => {
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(process.env.MONGO_URL, {
+        });
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('DB connection error:', error);
+    }
+}
+
+module.exports = { db };

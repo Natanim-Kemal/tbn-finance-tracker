ec48b42fc06bc5b2372d7c2ad9bb328a92d130c4
techkelster <techkelster@gmail.com>
2024-01-03 10:10:40 +0300

    React Setup Completed

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+// Render your React component instead
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <h1>Front End Started</h1>
+  </>
+);

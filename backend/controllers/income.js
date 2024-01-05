f1fcd4e5c216e97fde6702183112ce4e0b74ab86
Hanim07 <148555136+Hanim07@users.noreply.github.com>
2024-01-06 00:30:38 +0300

    Updated income.js

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -20,7 +20,7 @@ exports.addIncome = async (req, res) => {
         if(amount <= 0 || !amount === 'number'){
             return res.status(400).json({message: 'Amount must be a positive number!'})
         } 
-        // await income.save()
+        await income.save()
         res.status(200).json({message: 'Income Added'})
     } catch (error) {
         res.status(500).json({message: 'Server Error'})

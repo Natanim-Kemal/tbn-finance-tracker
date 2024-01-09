42f6e63c76f7f600643c2fe06c5759a91e6ef26c
Natanim-Kemal <kemalnatanim@gmail.com>
2024-01-09 19:03:09 +0300

    Middleware modules updated

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const xss = require('xss-clean');
 const cors = require('cors');
+const helmet = require('helmet')
 const { readdirSync } = require('fs');
 const mongoSanitize = require('express-mongo-sanitize');
 const cookieParser = require('cookie-parser');
@@ -8,11 +9,13 @@ const loggingMiddleware = require('./middlewares/loggingMiddleware')
 const rateLimitMiddleware = require('./middlewares/rateLimitMiddleware')
 const i18nMiddleware = require('./locales/i18nMiddleware');
 const errorHandler = require('./utils/errorHandler');
+
 const app = express();
 
 //middlewares 
 app.use(cookieParser());
 app.use(cors());
+app.use(helmet());
 app.use(errorHandler);
 app.use(rateLimitMiddleware);
 app.use(express.json());

c696beafd480c76ec4e6ad3888aedf763f563fc0
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-01-31 16:06:50 +0300

    income added

--- frontend/src/components/Income.jsx
+++ frontend/src/components/Income.jsx
@@ -1,100 +1,214 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./css/expenses.css";
+import trash from "../icons/trash.png";
 import Menu from "./Menu";
 import budgetIcon from "../icons/budge.png";
-import trash from "../icons/trash.png";
-import courseIcon from "../icons/courses.png";
 import dashboardIcon from "../icons/dashboard.png";
 import expenseIcon from "../icons/expense.png";
-import investmentIcon from "../icons/investmentIcon.png";
-import goalIcon from "../icons/goal.png";
-import settingIcon from "../icons/setting.png";
 import walletIcon from "../icons/wallet.png";
-import income from "./assest/income.png";
-import moreIcon from "../icons/moreIcon.jpg"
+import moreIcon from "../icons/moreIcon.jpg";
 import Buttons from "./buttons";
-import "./css/expenses.css";
-export default function Income() {
-    const menus = [
-        {id: 1,name: "Dashboard",active: false, iconSrc: dashboardIcon,},
-        { id: 3, name: "Budget", active: false, iconSrc: budgetIcon },
-        { id: 4, name: "Expense", active: false, iconSrc: expenseIcon },
-        { id: 5, name: "Wallet", active: false, iconSrc: walletIcon  },
-        { id: 6, name: "More", active: false, iconSrc:moreIcon },
-    ];
-    return(
-        <div>
-            <Menu  menus={menus} />
-            <div className="expense-contents">
-      <div className="header">
-        <h3>Expenses</h3>
-      </div>
-      <div className="total-exp">
-        <p>
-          Total Expense: <span>00000</span>
-        </p>
-      </div>
-      <div className="row main-under">
-        <div className="form col-md-6 col-lg-6 col-sm-12 expense-inputs">
-          <form>
-            <div class="form-group">
-              <input
-                type="text"
-                class="form-control"
-                id="input1"
-                placeholder="Expense title"
-              />
-            </div>
-            <div class="form-group">
-              <input
-                type="text"
-                class="form-control"
-                id="input2"
-                placeholder="Expense Amount"
-              />
-            </div>
-            <div class="form-group">
-              <input
-                type="text"
-                class="form-control"
-                id="input3"
-                placeholder="Enter A Date"
-              />
-            </div>
-            <div class="form-group">
-              <select class="form-control" id="selectOption">
-                <option value="">Option 1</option>
-              </select>
-            </div>
-            <div class="form-group">
-              <textarea
-                class="form-control"
-                id="textarea"
-                rows="3"
-                placeholder="Add a Reference"
-              ></textarea>
-            </div>
-            <Buttons content="Add Reference" onClick="" />
-          </form>
+
+const Expenses = () => {
+  const menus = [
+    { id: 1, name: "Dashboard", active: false, iconSrc: dashboardIcon },
+    { id: 3, name: "Budget", active: false, iconSrc: budgetIcon },
+    { id: 4, name: "Expense", active: false, iconSrc: expenseIcon },
+    { id: 5, name: "Wallet", active: false, iconSrc: walletIcon },
+    { id: 6, name: "More", active: false, iconSrc: moreIcon },
+  ];
+
+  const [formData, setFormData] = useState({
+    name: "",
+    amount: "",
+    category: "",
+    description: "",
+    date: "",
+  });
+
+  const [expenses, setExpenses] = useState([]);
+  const [fetchExpensesOnMount, setFetchExpensesOnMount] = useState(true);
+
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/getExpenses", {
+          credentials: "include",
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setExpenses(data);
+        console.log("Expenses fetched successfully:", data);
+      } catch (error) {
+        console.error("Error fetching expenses:", error.message);
+      }
+    };
+
+    if (fetchExpensesOnMount) {
+      // Call the function to fetch expenses when the component mounts
+      fetchExpenses();
+      setFetchExpensesOnMount(false);
+    }
+  }, [fetchExpensesOnMount]);
+
+  const calculateTotal = () => {
+    let totalExpenses = 0;
+    for (let exp of expenses) {
+      totalExpenses += exp.amount;
+    }
+    return <span>{totalExpenses}</span>;
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmission = async (e) => {
+    e.preventDefault();
+    const addExpense = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/addExpense", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          body: JSON.stringify({
+            name: formData.name,
+            amount: Number(formData.amount),
+            category: formData.category,
+            description: formData.description,
+            date: formData.date,
+          }),
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log("Expense added successfully:", data);
+        setExpenses((prevExpenses) => [...prevExpenses, data]);
+      } catch (error) {
+        console.error("Error adding expense:", error.message);
+      }
+    };
+
+    // Call the function to add the expense when the component mounts
+    addExpense();
+    console.log("Form data submitted", formData);
+  };
+
+  return (
+    <div className="exp-main col-md-9 col-lg-9 col-sm-12">
+      <Menu menus={menus} />
+      <div className="expense-contents">
+        <div className="header">
+          <h3>Income</h3>
         </div>
-        <div className="side-list col-md-6 col-lg-6 col-sm-12">
-          <div className="side-cont">
-            <div className="fir"></div>
-            <div className="sec">
-              <div className="sec-top">
-                <div className="elipse"></div>
-                <div>Rent</div>
+        <div className="total-exp">
+          <p>Total Income: {calculateTotal()}</p>
+        </div>
+        <div className="row main-under">
+          <div className="form col-md-6 col-lg-6 col-sm-12 expense-inputs">
+            <form>
+              <div class="form-group">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="input1"
+                  name="name"
+                  placeholder="Income title"
+                  onChange={handleChange}
+                />
               </div>
-              <div className="sec-bot">
-                <span className="amount">ETB 3000</span>
-                <span className="date">23/04/16</span>
-                <span className="note">Rent and Bill</span>
+              <div class="form-group">
+                <input
+                  type="number"
+                  class="form-control"
+                  name="amount"
+                  placeholder="Income Amount"
+                  onChange={handleChange}
+                />
               </div>
-            </div>
-            <img src={trash} />
+              <div class="form-group">
+                <input
+                  type="date"
+                  class="form-control"
+                  id="input3"
+                  name="date"
+                  placeholder="Enter A Date"
+                  onChange={handleChange}
+                />
+              </div>
+              <div class="form-group">
+                <select
+                  class="form-control"
+                  id="selectOption"
+                  name="category"
+                  onChange={handleChange}
+                >
+                  <option value="" disabled>
+                    Select Option
+                  </option>
+                  <option value="education">Education</option>
+                  <option value="groceries">Groceries</option>
+                  <option value="health">Health</option>
+                  <option value="subscriptions">Subscriptions</option>
+                  <option value="takeaways">Takeaways</option>
+                  <option value="clothing">Clothing</option>
+                  <option value="travelling">Travelling</option>
+                  <option value="other">Other</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <textarea
+                  class="form-control"
+                  id="textarea"
+                  name="description"
+                  rows="3"
+                  placeholder="Add a Reference"
+                  onChange={handleChange}
+                ></textarea>
+              </div>
+              <Buttons content="Add Reference" onClick={handleSubmission} />
+            </form>
           </div>
+          {expenses.length > 0 ? (
+            <div className="side-list col-md-6 col-lg-6 col-sm-12">
+              {expenses
+                .slice()
+                .reverse()
+                .map((expense) => (
+                  <div key={expense.id} className="side-cont">
+                    <div className="fir"></div>
+                    <div className="sec">
+                      <div className="sec-top">
+                        <div className="elipse"></div>
+                        <div>{expense.name}</div>
+                      </div>
+                      <div className="sec-bot">
+                        <span className="amount">{`ETB ${expense.amount}`}</span>
+                        <span className="date">{expense.date}</span>
+                        <span className="note">{expense.description}</span>
+                      </div>
+                    </div>
+                    <img src={trash} alt="Delete" />
+                  </div>
+                ))}
+            </div>
+          ) : (
+            <p>You haven't made any income</p>
+          )}
         </div>
       </div>
     </div>
-        </div>
-    );
-}
+  );
+};
+
+export default Expenses;
